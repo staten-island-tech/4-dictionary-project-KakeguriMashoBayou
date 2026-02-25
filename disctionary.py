94ad08f712bafd512112780ce9a3b12baee6069c
KakeguriMashoBayou <boyul@nycstudents.net>
2026-02-25 09:47:15 -0500

    Update disctionary.py

--- disctionary.py
+++ disctionary.py
@@ -1,6 +1,4 @@
-human = {
-    "age": 15,
-    "name": wilson, 
-    "height": 5
-
-}
+dollar_cent_store = dict(thing_1 = "chamoy_kit: 5$", thing_2 = "rusty_prosthetic_arms: 8$", thing_3 = "tin: 20$")
+print(dollar_cent_store)
+shopper = input("Buy something:")
+if buyer ==
