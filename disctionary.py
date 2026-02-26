c1631432ab60bfc3e394efb24f141883c99fd281
KakeguriMashoBayou <boyul@nycstudents.net>
2026-02-26 09:05:40 -0500

    h

--- disctionary.py
+++ disctionary.py
@@ -1,4 +1,5 @@
-dollar_cent_store = dict(thing_1 = "chamoy_kit: 5$", thing_2 = "rusty_prosthetic_arms: 8$", thing_3 = "tin: 20$")
+dollar_cent_store = dict[("chamoy kit",5), ("prosthetic_arms", 20), ("tin",20)]
 print(dollar_cent_store)
 shopper = input("Buy something:")
-if buyer ==
+if shopper == "chamoy kit":
+    print("5$")
