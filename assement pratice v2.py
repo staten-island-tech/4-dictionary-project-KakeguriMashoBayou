3cae793a1941d439e89f48c5c30b07cdd5731b9d
KakeguriMashoBayou <boyul@nycstudents.net>
2026-03-02 09:10:26 -0500

    fv

---
+++ assement pratice v2.py
@@ -0,0 +1,12 @@
+user = input("sentence: ").upper()
+char_to_count = "T"
+count = user.count(char_to_count) 
+print(count)
+count2 = user.count(char_to_count)
+char_to_count2 = "S "
+print(count)
+
+if count > count2:
+    print("Proabably English text")
+elif:
+    print("Probably French")
---
+++ assestment.py
@@ -0,0 +1,8 @@
+french = ["S", "s"]
+english = ["T", "t"]
+
+input("sentence")
+if french > english:
+    print ("This text is french")
+elif english > french :
+    print (" This text is english")
