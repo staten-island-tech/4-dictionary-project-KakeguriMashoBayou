ca4c18e217c22f7cc2d1c093244663ef5da77f70
KakeguriMashoBayou <boyul@nycstudents.net>
2026-03-09 09:22:26 -0400

    z

--- disctionary.py
+++ disctionary.py
@@ -1,7 +1,7 @@
 item = [
     {"name": "chamoy kit",
     "price": "$100000",
-    "department": "tktok",},
+    "department": "tiktok",},
 
 
     {"name": "prosthetic hand",
@@ -11,8 +11,26 @@ item = [
 
     {"name": "pickle",
     "price": "$10",
-    "deparment": "food"}
+    "department": "food"}
     ]
 print(item[0]["name"],item[0]["price"],item[0]["department"])
 print(item[1]["name"],item[1]["price"],item[1]["department"])
-print(item[2]["name"],item[2]["price"],item[2]["department"])
+print(item[2]["name"],item[2]["price"],item[2]["department"]) 
+
+buyer = input("what the hell do you want? ").upper()
+
+if buyer == "chamoy kit":
+    print("chamoy kit")
+elif buyer == "prosthetic hand":
+    print("prosthethic hand")
+elif buyer == "pickle":
+    print("pickle")
+
+
+while True:
+    user = input("are you going to buy more? yes/no")
+    if user == "no":
+        break
+    else: 
+        if user == "yes":
+            
