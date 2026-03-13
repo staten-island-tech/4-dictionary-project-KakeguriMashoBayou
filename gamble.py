b78cc4013de426cae5d55184b0dd6950cc2eb1c1
KakeguriMashoBayou <boyul@nycstudents.net>
2026-03-13 09:22:58 -0400

    z

--- gamble.py
+++ gamble.py
@@ -7,10 +7,23 @@ type = 1
 while coins > 0:
     coins = coins - 1
     played = played + 1
-    if type == 1:
+
+    if type ==1:
         machine = machine + 1
-    if machine == 35:
-        coins = coins + 30
-    type = 2
+        type =2 
+        if machine == 35:
+            coins = coins + 30
+            machine = 0
     elif type == 2:
-    
+        machine2 = machine2 + 1
+        type = 3
+        if machine2 == 100:
+            coins = coins+60
+            machine2 = 0
+    elif type == 3:
+        machine3=machine3 + 1
+        type = 1
+        if machine3 ==10:
+            coins = coins + 9
+            machine3=0
+print("Martha plays", played, "times before going to the streets and beg")                                                                        
